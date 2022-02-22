bc0986dc2c0a0733abbc3e31a3b6d7a669d80bcb
Mustafa <93796510+amustafaozkayam@users.noreply.github.com>
2022-02-22 22:17:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+
+var myNodelist = document.getElementsByTagName("LI");
+console.log(myNodelist);
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(e) {
+        if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('checked');
+            
+         }             
+        
+    },
+    false);
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You should write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+let addButton = document.querySelector(".addBtn");
+addButton.addEventListener("click", newElement);
+window.addEventListener("keyup", function(event) {
+    if (event.code == "Enter" || event.code == "NumpadEnter") { newElement(event.code); }
+  });
+
+
+
+
+
